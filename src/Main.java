60c3c4605d709dd512792928fa65c319988e97f7
Kamerron Pace <youngkamman@gmail.com>
2022-08-23 19:18:40 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+//        Player player = new Player();
+//        player.name ="Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("Kamerron", 50, "Sword");
+        System.out.println("Intial health is " + player.getHealth());
+
+    }
+}
